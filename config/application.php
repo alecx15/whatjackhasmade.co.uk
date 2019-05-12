9c4b25216421a476ec054cd21d742729b704b784
Jack Pritchard <jack@noface.co.uk>
2019-05-12 05:52:41 +0100

    Set relative log file path

--- config/application.php
+++ config/application.php
@@ -102,7 +102,7 @@ Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', 0);
 //Enable error logging.
 @ini_set('log_errors', 'On');
-@ini_set('error_log', 'C:/xampp/htdocs/whatjackhasmade.co.uk/web/app/elm-error-logs/php-errors.log');
+@ini_set('error_log', $webroot_dir . '/app/elm-error-logs/php-errors.log');
 
 //Don't show errors to site visitors.
 @ini_set('display_errors', 'Off');
